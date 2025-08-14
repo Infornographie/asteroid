fdc10ba47a9c90cb921c9e3ffcd967723022ac62
Infornographie <nutellaman@gmail.com>
2025-08-14 11:16:45 +0200

    Grouped drawables and updatables

--- main.py
+++ main.py
@@ -11,20 +11,31 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     clock = pygame.time.Clock()
-    dt = 0
+    dt = 1 / 60  
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    
+    Player.containers = updatables, drawables
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while pygame.get_init():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        player.draw(screen)
-        pygame.display.flip()
-        clock.tick(60)  # Limit to 60 FPS
         
+        screen.fill((0, 0, 0))
+        updatables.update(dt)
+        for obj in drawables:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(60)  # Limit to 60 FPS      
 
 if __name__ == "__main__":
     main()
