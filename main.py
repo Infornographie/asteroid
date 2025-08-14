3a74ebe57e2afbc3a8342828bfbf555aacff7abb
Infornographie <nutellaman@gmail.com>
2025-08-14 14:41:32 +0200

    Now there's some sort of asteroids! And you can lose. It's a game, right?

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     print("Starting Asteroids!")
@@ -19,10 +22,14 @@ def main():
 
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
     Player.containers = updatables, drawables
+    Asteroids.containers = asteroids, updatables, drawables
+    AsteroidField.containers = updatables
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while pygame.get_init():
         for event in pygame.event.get():
@@ -34,6 +41,13 @@ def main():
         for obj in drawables:
             obj.draw(screen)
 
+        # Check for collisions between player and asteroids
+        for asteroid in asteroids:
+            if player.collides(asteroid):
+                print("Game over!")
+                pygame.quit()
+                sys.exit()
+                
         pygame.display.flip()
         clock.tick(60)  # Limit to 60 FPS      
 
